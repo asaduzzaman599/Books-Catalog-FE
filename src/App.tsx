50f96cb0723174b35e3112c2db1ba090b3e7b816
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2023-07-16 14:24:47 +0600

    [DONE] shadcn ui added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "./components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1 className='bg-red-500 text-white'>Project Running!</h1>
+      <Button variant='destructive'>Click me</Button>
     </>
   )
 }
